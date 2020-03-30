b1bc63a3afd5488b0c8dd949884b7f21044cda5d
Eric-Lombardo <ejlanim8or@gmail.com>
2020-03-30 19:22:37 +0000

    exercise 4 completed

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,36 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+
+store4 = Store.create(
+  name: "Surrey",
+  annual_revenue: 224000,
+  mens_apparel: false,
+  womens_apparel: true,
+)
+
+store5 = Store.create(
+  name: "Whistler",
+  annual_revenue: 1900000,
+  mens_apparel: true,
+  womens_apparel: false,
+)
+
+store6 = Store.create(
+  name: "Yaletown",
+  annual_revenue: 430000,
+  mens_apparel: true,
+  womens_apparel: true,
+)
+
+@mens_stores = Store.where(mens_apparel: true)
+for store in @mens_stores
+  puts "#{store.name} makes #{store.annual_revenue}"
+end
+
+@womens_stores = Store.where(womens_apparel: true)
+for store in @womens_stores
+  if store.annual_revenue < 1000000
+    puts "#{store.name} makes less than 1M$"
+  end
+end
