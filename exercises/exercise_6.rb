38b0dcbc5472655bc93a0b7195642a21d4523497
Eric-Lombardo <ejlanim8or@gmail.com>
2020-03-30 19:50:43 +0000

    exercise 6 completed

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,18 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Eric", last_name: "L", hourly_rate: 20)
+@store1.employees.create(first_name: "stew", last_name: "katz", hourly_rate: 20)
+@store1.employees.create(first_name: "sylvia", last_name: "palmer", hourly_rate: 30)
+@store1.employees.create(first_name: "victor", last_name: "lee", hourly_rate: 40)
+
+@store2.employees.create(first_name: "shawn", last_name: "evans", hourly_rate: 40)
+@store2.employees.create(first_name: "laura", last_name: "brown", hourly_rate: 20)
+@store2.employees.create(first_name: "sam", last_name: "smith", hourly_rate: 20)
+@store2.employees.create(first_name: "chelsea", last_name: "handler", hourly_rate: 20)
+@store2.employees.create(first_name: "san", last_name: "tiago", hourly_rate: 90)
+
+puts "THE AMOUNT OF EMPLOYEES: #{Employee.count}"
+
